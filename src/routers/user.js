4105db914eacf03a316b8ae41954c1ae19da9ed8
Aamer Desai <56724460+minm333@users.noreply.github.com>
2020-11-11 05:47:03 -0500

    Add logout route

--- src/routers/user.js
+++ src/routers/user.js
@@ -31,6 +31,34 @@ router.post('/users/login', async (req, res) => {
     }
 });
 
+// User Logout
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        // Remove current token from the list of tokens
+        req.user.tokens = req.user.tokens.filter(
+            (token) => token.token !== req.token
+        );
+        await req.user.save();
+
+        res.send();
+    } catch (error) {
+        res.status(500).send();
+    }
+});
+
+// User Logout All Sessions
+router.post('/users/logoutall', auth, async (req, res) => {
+    try {
+        // Remove current token from the list of tokens
+        req.user.tokens = [];
+        await req.user.save();
+
+        res.send();
+    } catch (error) {
+        res.status(500).send();
+    }
+});
+
 // Get all users
 router.get('/users/me', auth, async (req, res) => {
     res.send(req.user);
