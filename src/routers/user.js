35beac796c8eac904565ff42c5547d278deef2e6
Aamer Desai <56724460+minm333@users.noreply.github.com>
2020-11-12 08:01:27 -0500

    Add avatar serving route

--- src/routers/user.js
+++ src/routers/user.js
@@ -143,5 +143,19 @@ router.delete('/users/me/avatar', auth, async (req, res) => {
     res.send();
 });
 
+// Fetch user avatar
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user || !user.avatar) {
+            throw new Error();
+        }
+        res.set('Content-Type', 'image/jpg');
+        res.send(user.avatar);
+    } catch (error) {
+        res.status(404).send();
+    }
+});
+
 // Export user routes
 module.exports = router;
