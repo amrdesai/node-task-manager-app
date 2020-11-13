baff1bc95629dcd658193f3e5dfecdf456d1356e
Aamer Desai <56724460+minm333@users.noreply.github.com>
2020-11-12 22:59:43 -0500

    Fix minor bugs related to auth and secret

--- src/models/user.js
+++ src/models/user.js
@@ -87,7 +87,10 @@ userSchema.methods.toJSON = function () {
 // Auth Token
 userSchema.methods.generateAuthToken = async function () {
     const user = this;
-    const token = jwt.sign({ _id: user._id.toString() }, 'thisisthesecretcode');
+    const token = jwt.sign(
+        { _id: user._id.toString() },
+        process.env.JWT_SECRET
+    );
 
     user.tokens = user.tokens.concat({ token });
     await user.save();
